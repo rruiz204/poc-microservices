6a3673dd4fea837ae707239ee72199111e48dd1a
roney ruiz <roneyruizrojas@gmail.com>
2024-12-04 16:07:41 -0600

    chore: remove unnecessary code

--- services/service-a/src/controller.ts
+++ services/service-a/src/controller.ts
@@ -4,12 +4,5 @@ const index = async (req: Request, res: Response) => {
   res.status(200).json({ path: "index", service: "express" });
 };
 
-const config = async (req: Request, res: Response) => {
-  res.status(200).json({
-    path: "config",
-    consul: `Host: ${process.env.CONSULT_HOST} | Port : ${parseInt(process.env.CONSULT_PORT!, 10)}`
-  });
-};
-
-const Controller = Object.freeze({ index, config });
+const Controller = Object.freeze({ index });
 export default Controller;
--- services/service-a/src/router.ts
+++ services/service-a/src/router.ts
@@ -4,6 +4,5 @@ import Controller from "./controller";
 const ApiRouter = Router();
 
 ApiRouter.get("/", Controller.index);
-ApiRouter.get("/config", Controller.config);
 
 export default ApiRouter;
