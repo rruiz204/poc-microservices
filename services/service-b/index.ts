b2e0692c9e25a53ebacaf5ff93b41c576efed557
roney ruiz <roneyruizrojas@gmail.com>
2024-12-20 17:01:11 -0600

    feat(b): modify index and create bootstrap file

--- services/service-b/index.ts
+++ services/service-b/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { ApiRouter } from "./src/router";
+import { Bootstrap } from "./src/bootstrap";
+
+import { HealthChecker } from "@consul/health";
+import { ServiceConfig } from "@config/service";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", ApiRouter);
+app.use("/health", HealthChecker);
+
+app.listen(ServiceConfig.port, () => {
+  const url = `http://${ServiceConfig.address}:${ServiceConfig.port}`;
+  console.log(`Server is listening at ${url}`);
+});
+
+await Bootstrap();
