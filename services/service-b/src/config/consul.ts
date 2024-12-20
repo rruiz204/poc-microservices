111822b936125538d63df46d757b3d74e86837b1
roney ruiz <roneyruizrojas@gmail.com>
2024-12-20 15:09:12 -0600

    feat: modify cofigurations and install dependencies

--- services/service-b/src/config/consul.ts
+++ services/service-b/src/config/consul.ts
@@ -1,15 +1,14 @@
 const {
-  CONSUL_HOST = "consul",
-  CONSUL_PORT = 8500,
+  CONSUL_HOST,
+  CONSUL_PORT,
 } = process.env;
-
 
 const CONSUL_HOST_PARSED = `http://${CONSUL_HOST}:${CONSUL_PORT}`;
 
-const CONSUL_PORT_PARSED = typeof(CONSUL_PORT) == "number" ? CONSUL_PORT : parseInt(CONSUL_PORT, 10);
+const CONSUL_PORT_PARSED = parseInt(CONSUL_PORT!, 10);
 
 
-const ConsulConfig = {
+export const ConsulConfig = {
   host: CONSUL_HOST_PARSED,
   port: CONSUL_PORT_PARSED
 };
--- services/service-b/src/config/service.ts
+++ services/service-b/src/config/service.ts
@@ -1,22 +1,22 @@
 import { v4 } from "uuid";
 
 const {
-  EXPRESS_HOST = "service-express-b",
-  EXPRESS_PORT = 4000,
+  EXPRESS_HOST,
+  EXPRESS_PORT,
 } = process.env;
 
 const UUID = v4();
 
 const EXPRESS_HOST_PARSED = `http://${EXPRESS_HOST}:${EXPRESS_PORT}`;
 
-const EXPRESS_PORT_PARSED = typeof(EXPRESS_PORT) == "number" ? EXPRESS_PORT : parseInt(EXPRESS_PORT, 10);
+const EXPRESS_PORT_PARSED = parseInt(EXPRESS_PORT!, 10);
 
 const CHECKER_NAME = `${UUID}-health-check`;
 
 const CHECKER_HTTP = `http://${EXPRESS_HOST}:${EXPRESS_PORT}/health`;
 
 
-const ServiceConfig = {
+export const ServiceConfig = {
   id: UUID,
   name: "express-service-b",
   host: EXPRESS_HOST_PARSED,
@@ -26,5 +26,3 @@ const ServiceConfig = {
     http: CHECKER_HTTP,
   },
 };
-
-export default ServiceConfig;
